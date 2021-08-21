1cd115baf8e2a1644f49204ff1110c2ef3eb678c
ealzate <ealzate@pratechgroup.com>
2021-08-21 13:23:14 -0500

    express basico con typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hola Typescript Edison')
+import express from 'express';
+
+const app = express();
+const port =8001;
+ 
+app.get('/', (req, res)=> {
+    res.send('Servidor con Typescript');
+});
+ 
+app.listen(port,()=>{
+    return console.log(`Server corriendo en puerto : ${port}`)
+});
